9222fbb795fd4821bb626919184a9acbdff24400
Chilinhung <ansleyh125@gmail.com>
2024-09-25 12:45:32 +0800

    add question answers detection function and change sample image RWD

--- cnt_db.js
+++ cnt_db.js
@@ -2,21 +2,15 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const { Pool } = require("pg");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 /* SQL
 ALTER TABLE survey_results
 ADD COLUMN ip_address VARCHAR(255),
 ADD COLUMN submit_time TIMESTAMP;
 */
-
-// tackle root URL GET requests
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "questions.html"));
-});
-
-const { Pool } = require("pg");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -32,6 +26,10 @@ const pool = new Pool({
         }
       : false,
 });
+// tackle root URL GET requests
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "questions.html"));
+});
 
 app.post("/submit", async (req, res) => {
   const responses = req.body.responses; // 獲取前端發送的 responses
@@ -39,10 +37,18 @@ app.post("/submit", async (req, res) => {
   const ipAddress = req.ip;
   const submitTime = new Date().toISOString();
   try {
-    
     for (const response of responses) {
       const { questionId, aspect, value, gender, age, user_text } = response;
-      await saveResponse(questionId, aspect, value, ipAddress, submitTime, gender, age, user_text);
+      await saveResponse(
+        questionId,
+        aspect,
+        value,
+        ipAddress,
+        submitTime,
+        gender,
+        age,
+        user_text
+      );
     }
     res.send({ status: "success", message: "All responses saved!" });
   } catch (error) {
@@ -83,6 +89,33 @@ async function saveResponse(
     throw error;
   }
 }
+
+
+app.get("/api/get_counts", async (req, res) => {
+  try {
+    const result = await pool.query(
+      "SELECT question_id, COUNT(*) as response_count FROM survey_results GROUP BY question_id"
+    );
+    let counts = {};
+    result.rows.forEach((row) => {
+      let setId = row.question_id.split("_")[0]; // Assuming question_id starts with set id like 'set_1_q1'
+      console.log("current search for count is set", setId);
+      if (counts[setId]) {
+        counts[setId] += 1;
+      } else {
+        counts[setId] = 1;
+      }
+    });
+    // Normalize counts by number of questions per set if necessary
+    Object.keys(counts).forEach((setId) => {
+      counts[setId] /= 40; // Adjust this if the number of questions per set varies
+    });
+    res.json(counts);
+  } catch (error) {
+    console.error("Error fetching counts:", error);
+    res.status(500).send({ status: "error", message: "Error fetching counts" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
